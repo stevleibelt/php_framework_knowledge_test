f96568ef7d0bbd7334ea7cb5bd124c8363e37bea
stevleibelt <stev@leibelt.de>
2013-05-25 22:58:42 +0200

    Finished answer factories

---
+++ src/Net/Bazzline/KnowledgeTest/Factory/AnswerFromPhpArrayFactory.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-05-26
+ */
+namespace Net\Bazzline\KnowledgeTest\Factory;
+
+use Net\Bazzline\KnowledgeTest\TestCase\SelectMultipleAnswer;
+use Net\Bazzline\KnowledgeTest\TestCase\SelectSingleAnswer;
+use Net\Bazzline\KnowledgeTest\TestCase\FreeTextAnswer;
+
+/**
+ * Class AnswerFromPhpArrayFactory
+ *
+ * @package Net\Bazzline\KnowledgeTest\Factory
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-05-26
+ */
+class AnswerFromPhpArrayFactory implements FactoryInterface
+{
+    /**
+     * Creates object
+     *
+     * @param mixed $source - the source
+     *  example:
+     *      array(
+     *          'type' => 'SelectMultipleAnswer',
+     *          'opportunities' => array(
+     *              'First Answer',
+     *              'Second Answer',
+     *              'Third Answer',
+     *              'Fourth Answer'
+     *          ),
+     *          'validOpportunities' => array(
+     *              'Second Answer',
+     *              'Third Answer'
+     *          )
+     *      )
+     *
+     * @return \Net\Bazzline\KnowledgeTest\TestCase\Answer
+     * @throws FactoryInvalidArgumentException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-26
+     */
+    public function fromSource($source)
+    {
+        if (!is_array($source)) {
+            throw new FactoryInvalidArgumentException(
+                'Source has to be from type array'
+            );
+        }
+
+        if (!isset($source['type'])) {
+            throw new FactoryInvalidArgumentException(
+                'No type found in source array'
+            );
+        }
+
+        if (!isset($source['opportunities'])) {
+            throw new FactoryInvalidArgumentException(
+                'No opportunities found in source array'
+            );
+        }
+
+        if (!isset($source['validOpportunities'])) {
+            throw new FactoryInvalidArgumentException(
+                'No valid opportunities found in source array'
+            );
+        }
+
+        if (!class_exists('Net\\Bazzline\\KnowledgeTest\\TestCase\\' . $source['type'])) {
+            throw new FactoryInvalidArgumentException(
+                'Not supported type found in source array'
+            );
+        }
+
+        $answer = new $source['type']();
+        foreach ($source['opportunities'] as $opportunity) {
+            $answer->addOpportunity($opportunity);
+        }
+        foreach ($source['validOpportunities'] as $validOpportunity) {
+            $answer->addValidOpportunity($validOpportunity);
+        }
+
+        return $answer;
+    }
+}
