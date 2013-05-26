b866077ca9cc2f9e9878596f8858dac561300636
stevleibelt <stev@leibelt.de>
2013-05-27 00:32:00 +0200

    Fixed bug in servicelocator.

--- src/Net/Bazzline/KnowledgeTest/ServiceLocator/ServiceLocator.php
+++ src/Net/Bazzline/KnowledgeTest/ServiceLocator/ServiceLocator.php
@@ -5,26 +5,8 @@
  */
 namespace Net\Bazzline\KnowledgeTest\ServiceLocator;
 
-use Net\Bazzline\KnowledgeTest\Command\TestCommand;
-use Net\Bazzline\KnowledgeTest\Factory\AnswerFromPhpArrayFactory;
-use Net\Bazzline\KnowledgeTest\Factory\AnswerFromXmlFactory;
-use Net\Bazzline\KnowledgeTest\Factory\QuestionFromPhpArrayFactory;
-use Net\Bazzline\KnowledgeTest\Factory\QuestionFromXmlFactory;
-use Net\Bazzline\KnowledgeTest\Factory\TestCaseFactory;
-use Net\Bazzline\KnowledgeTest\Factory\TestCaseFromXmlFactory;
-use Net\Bazzline\KnowledgeTest\Factory\TestCaseFromPhpArrayFactory;
-use Net\Bazzline\KnowledgeTest\Factory\SuiteFactory;
-use Net\Bazzline\KnowledgeTest\Factory\SuiteFromPhpArrayFactory;
-use Net\Bazzline\KnowledgeTest\Factory\SuiteFromXmlFactory;
-use Net\Bazzline\KnowledgeTest\Filesystem\SuiteFilterDirectoryIterator;
-use Net\Bazzline\KnowledgeTest\Filesystem\TestCaseFilterDirectoryIterator;
-use Net\Bazzline\KnowledgeTest\TestCase\Suite;
-use Net\Bazzline\KnowledgeTest\TestCase\TestCase;
-use Net\Bazzline\KnowledgeTest\TestCase\Question;
-use Net\Bazzline\KnowledgeTest\TestCase\SingleAnswer;
-use Net\Bazzline\KnowledgeTest\TestCase\MultipleAnswer;
-use Net\Bazzline\KnowledgeTest\TestCase\FreeTextAnswer;
 use DirectoryIterator;
+use InvalidArgumentException;
 
 /**
  * Class ServiceLocator
@@ -52,177 +34,177 @@ class ServiceLocator
     }
 
     /**
-     * @return AnswerFromPhpArrayFactory
+     * @return \Net\Bazzline\KnowledgeTest\Factory\AnswerFromPhpArrayFactory
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getAnswerFromPhpArrayFactory()
     {
-        return $this->getFromInstancePoolOrCreate('AnswerFromPhpArrayFactory');
+        return $this->getFromInstancePoolOrCreate('\\Net\\Bazzline\\KnowledgeTest\\Factory\\AnswerFromPhpArrayFactory');
     }
 
 
     /**
-     * @return AnswerFromXmlFactory
+     * @return \Net\Bazzline\KnowledgeTest\Factory\AnswerFromXmlFactory
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getAnswerFromXmlFactory()
     {
-        return $this->getFromInstancePoolOrCreate('AnswerFromXmlFactory');
+        return $this->getFromInstancePoolOrCreate('\\Net\\Bazzline\\KnowledgeTest\\Factory\\AnswerFromXmlFactory');
     }
 
 
     /**
-     * @return QuestionFromPhpArrayFactory
+     * @return \Net\Bazzline\KnowledgeTest\Factory\QuestionFromPhpArrayFactory
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getQuestionFromPhpArrayFactory()
     {
-        return $this->getFromInstancePoolOrCreate('QuestionFromPhpArrayFactory');
+        return $this->getFromInstancePoolOrCreate('\\Net\\Bazzline\\KnowledgeTest\\Factory\\QuestionFromPhpArrayFactory');
     }
 
 
     /**
-     * @return QuestionFromXmlFactory
+     * @return \Net\Bazzline\KnowledgeTest\Factory\QuestionFromXmlFactory
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getQuestionFromXmlFactory()
     {
-        return $this->getFromInstancePoolOrCreate('QuestionFromXmlFactory');
+        return $this->getFromInstancePoolOrCreate('\\Net\\Bazzline\\KnowledgeTest\\Factory\\QuestionFromXmlFactory');
     }
 
     /**
-     * @return SuiteFactory
+     * @return \Net\Bazzline\KnowledgeTest\Factory\SuiteFactory
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getSuiteFactory()
     {
-        return $this->getFromInstancePoolOrCreate('SuiteFactory');
+        return $this->getFromInstancePoolOrCreate('\\Net\\Bazzline\\KnowledgeTest\\Factory\\SuiteFactory');
     }
 
 
     /**
-     * @return TestCaseFactory
+     * @return \Net\Bazzline\KnowledgeTest\Factory\TestCaseFactory
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getTestCaseFactory()
     {
-        return $this->getFromInstancePoolOrCreate('TestCaseFactory');
+        return $this->getFromInstancePoolOrCreate('\\Net\\Bazzline\\KnowledgeTest\\Factory\\TestCaseFactory');
     }
 
     /**
-     * @return TestCaseFromXmlFactory
+     * @return \Net\Bazzline\KnowledgeTest\Factory\TestCaseFromXmlFactory
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getTestCaseFromXmlFactory()
     {
-        return $this->getFromInstancePoolOrCreate('TestCaseFromXmlFactory');
+        return $this->getFromInstancePoolOrCreate('\\Net\\Bazzline\\KnowledgeTest\\Factory\\TestCaseFromXmlFactory');
     }
 
     /**
-     * @return TestCaseFromPhpArrayFactory
+     * @return \Net\Bazzline\KnowledgeTest\Factory\TestCaseFromPhpArrayFactory
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getTestCaseFromPhpArrayFactory()
     {
-        return $this->getFromInstancePoolOrCreate('TestCaseFromPhpArrayFactory');
+        return $this->getFromInstancePoolOrCreate('\\Net\\Bazzline\\KnowledgeTest\\Factory\\TestCaseFromPhpArrayFactory');
     }
 
     /**
-     * @return SuiteFromPhpArrayFactory
+     * @return \Net\Bazzline\KnowledgeTest\Factory\SuiteFromPhpArrayFactory
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getSuiteFromPhpArrayFactory()
     {
-        return $this->getFromInstancePoolOrCreate('SuiteFromPhpArrayFactory');
+        return $this->getFromInstancePoolOrCreate('\\Net\\Bazzline\\KnowledgeTest\\Factory\\SuiteFromPhpArrayFactory');
     }
 
     /**
-     * @return SuiteFromXmlFactory
+     * @return \Net\Bazzline\KnowledgeTest\Factory\SuiteFromXmlFactory
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getSuiteFromXmlFactory()
     {
-        return $this->getFromInstancePoolOrCreate('SuiteFromXmlFactory');
+        return $this->getFromInstancePoolOrCreate('\\Net\\Bazzline\\KnowledgeTest\\Factory\\SuiteFromXmlFactory');
     }
 
     /**
-     * @return Suite
+     * @return \Net\Bazzline\KnowledgeTest\TestCase\Suite
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getNewSuite()
     {
-        return new Suite();
+        return new \Net\Bazzline\KnowledgeTest\TestCase\Suite();
     }
 
     /**
-     * @return TestCase
+     * @return \Net\Bazzline\KnowledgeTest\TestCase\TestCase
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getNewTestCase()
     {
-        return new TestCase();
+        return new \Net\Bazzline\KnowledgeTest\TestCase\TestCase();
     }
 
     /**
-     * @return Question
+     * @return \Net\Bazzline\KnowledgeTest\TestCase\Question
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getNewQuestion()
     {
-        return new Question();
+        return new \Net\Bazzline\KnowledgeTest\TestCase\Question();
     }
 
     /**
-     * @return SingleAnswer
+     * @return \Net\Bazzline\KnowledgeTest\TestCase\SingleAnswer
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getNewSingleAnswer()
     {
-        return new SingleAnswer();
+        return new \Net\Bazzline\KnowledgeTest\TestCase\SingleAnswer();
     }
 
     /**
-     * @return MultipleAnswer
+     * @return \Net\Bazzline\KnowledgeTest\TestCase\MultipleAnswer
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getNewMultipleAnswer()
     {
-        return new MultipleAnswer();
+        return new \Net\Bazzline\KnowledgeTest\TestCase\MultipleAnswer();
     }
 
     /**
-     * @return FreeTextAnswer
+     * @return \Net\Bazzline\KnowledgeTest\TestCase\FreeTextAnswer
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getNewFreeTextAnswer()
     {
-        return new FreeTextAnswer();
+        return new \Net\Bazzline\KnowledgeTest\TestCase\FreeTextAnswer();
     }
 
     /**
-     * @return TestCommand
+     * @return \Net\Bazzline\KnowledgeTest\Command\TestCommand
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getNewTestCommand()
     {
-        $testCommand = new TestCommand();
+        $testCommand = new \Net\Bazzline\KnowledgeTest\Command\TestCommand();
         $testCommand->setServiceLocator($this);
 
         return $testCommand;
@@ -241,24 +223,24 @@ class ServiceLocator
 
     /**
      * @param $path
-     * @return SuiteFilterDirectoryIterator
+     * @return \Net\Bazzline\KnowledgeTest\Filesystem\SuiteFilterDirectoryIterator
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getNewSuiteFilterDirectoryIterator($path)
     {
-        return new SuiteFilterDirectoryIterator($this->getDirectoryIterator($path));
+        return new \Net\Bazzline\KnowledgeTest\Filesystem\SuiteFilterDirectoryIterator($this->getDirectoryIterator($path));
     }
 
     /**
      * @param $path
-     * @return TestCaseFilterDirectoryIterator
+     * @return \Net\Bazzline\KnowledgeTest\Filesystem\TestCaseFilterDirectoryIterator
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-05-26
      */
     public function getNewTestCaseFilterDirectoryIterator($path)
     {
-        return new TestCaseFilterDirectoryIterator($this->getDirectoryIterator($path));
+        return new \Net\Bazzline\KnowledgeTest\Filesystem\TestCaseFilterDirectoryIterator($this->getDirectoryIterator($path));
     }
 
     /**
@@ -269,11 +251,16 @@ class ServiceLocator
      */
     private function getFromInstancePoolOrCreate($className)
     {
-        if (!isset($this->instancePool[$className])) {
+        if (!isset($this->instancePool[$className])
+            && (class_exists($className))) {
             $this->instancePool[$className] = new $className();
-            if (in_array('ServiceLocatorAwareInterface', get_declared_interfaces($className))) {
+            if (in_array('ServiceLocatorAwareInterface', class_implements($this->instancePool[$className]))) {
                 $this->instancePool[$className]->setServiceLocator($this);
             }
+        } else {
+            throw new InvalidArgumentException(
+                'class name "' . $className . '" not available.'
+            );
         }
 
         return $this->instancePool[$className];
