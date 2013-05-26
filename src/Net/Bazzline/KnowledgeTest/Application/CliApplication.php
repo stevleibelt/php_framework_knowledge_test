a5f7954534cc5fe365f79a25b9a86f7cac134443
stevleibelt <stev@leibelt.de>
2013-05-26 21:29:05 +0200

    Added service locator

--- src/Net/Bazzline/KnowledgeTest/Application/CliApplication.php
+++ src/Net/Bazzline/KnowledgeTest/Application/CliApplication.php
@@ -5,6 +5,7 @@
  */
 namespace Net\Bazzline\KnowledgeTest\Application;
 
+use Net\Bazzline\KnowledgeTest\ServiceLocator\ServiceLocator;
 use Symfony\Component\Console\Application as SymfonyApplication;
 
 /**
@@ -29,11 +30,22 @@ class CliApplication extends SymfonyApplication
      * @since 2013-05-26
      */
     const VERSION = '1.0.0';
+    /**
+     * @var ServiceLocator;
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-26
+     */
+    private $serviceLocator;
 
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-26
+     */
     public function __construct()
     {
         parent::__construct(self::NAME, self::VERSION);
 
+        $this->serviceLocator = new ServiceLocator();
         //$this->add(new ManualCommand());
     }
 }
