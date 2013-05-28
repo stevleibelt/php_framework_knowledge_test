070c2d48b0ecdb88d3979c65bc6bfecee80b24f3
stevleibelt <stev@leibelt.de>
2013-05-28 15:44:19 +0200

    Renamved src to source

--- bin/net_bazzline_framework_knowledge_test.php
+++ bin/net_bazzline_framework_knowledge_test.php
@@ -10,11 +10,11 @@ chdir(realpath(getcwd()));
 require 'vendor/autoload.php';
 
 //autloader for development
-if (file_exists('src/Net/Bazzline/KnowledgeTest/basicAutoloader.php')) {
+if (file_exists('source/Net/Bazzline/KnowledgeTest/basicAutoloader.php')) {
     echo 'Development mode.' . PHP_EOL;
     echo 'Loading autoloaders' . PHP_EOL;
 
-    require 'src/Net/Bazzline/KnowledgeTest/basicAutoloader.php';
+    require 'source/Net/Bazzline/KnowledgeTest/basicAutoloader.php';
 }
 
 $application = new \Net\Bazzline\KnowledgeTest\Application\CliApplication();
